de7243cdd4356f650b3b56c7aaa08345a4fc9842
ouwen.xing <ouwen.xxing@gmail.com>
2016-10-26 22:26:39 +0800

    shadowsocks linux automation script

---
+++ ss_install_ubuntu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sspath=$(which ssserver)
+if [[ "" == $sspath ]]; then
+  echo "installing shadowsocks..." 
+  /usr/bin/pip install shadowsocks -q
+fi
+
+ip=$(ifconfig | grep -A 1 "eth1" | grep -v eth1 | cut -d':' -f2 | cut -d' ' -f1)
+echo "ip is: $ip"
+
+echo '{
+    "server": "'$ip'",
+    "port_password": {
+        "8888": "alivps"
+    },
+    "timeout": 300,
+    "method": "aes-256-cfb"
+}' > /etc/shadowsocks.json
+
+pid=$(ps -aux | grep ssserver | grep -v grep | cut -d' ' -f7)
+if [[ "" != $pid ]]; then 
+  echo "killing the current process $pid..."
+  kill $pid
+fi
+/usr/local/bin/ssserver -c /etc/shadowsocks.json > /var/log/ssserver.log 2>&1 &
+
