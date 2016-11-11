fb5da2e783f24943190c250402f02f4ed559fe5e
Ouwen Xing <ouwen.xxing@gmail.com>
2016-11-11 22:10:42 +0800

    maintain script for reload ss

---
+++ ss_maintain.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+/bin/cp -f /root/ss_config/shadowsocks.json.template /root/ss_config/shadowsocks.json.tmp
+users=$(cat /root/ss_config/users-pwd.txt | cut -d'#' -f1)
+for user in $users
+do
+  sed -i '4 i \ \ \ \ \ \ \ \ '$user /root/ss_config/shadowsocks.json.tmp
+done
+cat /root/ss_config/shadowsocks.json.tmp > /etc/shadowsocks.json
+rm /root/ss_config/shadowsocks.json.tmp
+pid=$(ps -aux | grep ssserver | grep -v grep | cut -d' ' -f7)
+if [[ "" != $pid ]]; then
+  echo "ssserver id is: $pid, killing it..."
+  kill $pid
+fi
+/usr/local/bin/ssserver -c /etc/shadowsocks.json > /var/log/ssserver.log 2>&1 &
